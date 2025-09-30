829d3ef3b183439a39229959e2ad3812ca55b02a
ThiagoSilsa <thiagosantosprog@gmail.com>
2025-09-30 20:21:43 -0300

    fix(Header/SideBar): Fixing bug when open a modal, sidebar stills clear

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,22 +1,24 @@
 "use client";
 import Link from "next/link";
-import {
-  LayoutDashboard,
-  ClipboardList,
-  Plus,
-  Utensils,
-  CreditCard,
-  Settings,
-  Shield,
-  X,
-} from "lucide-react";
+import { LayoutDashboard, Utensils, Settings } from "lucide-react";
+
+import { BiFoodMenu, BiCheckShield } from "react-icons/bi";
+
+import { IoIosHelpCircleOutline } from "react-icons/io";
+
 import { cn } from "@/lib/utils";
 
 const navigation = [
-  { name: "Dashboard", href: "/home", icon: LayoutDashboard },
-  { name: "Mesas", href: "/mesas", icon: Utensils },
-  { name: "Admin Cardápio", href: "/admin/cardapio", icon: Shield },
+  { name: "Dashboard", href: "/dashboard", icon: LayoutDashboard },
+  { name: "Mesas", href: "/tables", icon: Utensils },
+  { name: "Catálogo", href: "/menu", icon: BiFoodMenu },
+
+  //Configuraçções do aplicativo, interface, etc, app em coluna, cores, etc.
   { name: "Configurações", href: "/config", icon: Settings },
+  // Menu de administrador, condifguração de 10%, cadastro e exclusão de fujncionários,
+  // Recebimento, número de mesas.
+  { name: "Administrativo", href: "/admin", icon: BiCheckShield },
+  { name: "Suporte", href: "/help", icon: IoIosHelpCircleOutline },
 ];
 
 export default function Sidebar({
@@ -30,12 +32,14 @@ export default function Sidebar({
       {/* Sidebar */}
       <aside
         className={cn(
-          "fixed top-14 left-0 z-50 h-full border-r border-border text-[var(--light-text1)]",
+          "fixed top-14 left-0 z-3 h-full border-r border-border text-[var(--light-text1)]",
           "bg-[var(--light-bg1)]",
           isSidebarOpen
-            ? "w-64"
+            ? "w-64 transition-discrete duration-300 ease-out"
             : "w-16 transition-discrete duration-300 ease-out"
         )}
+        onMouseEnter={() => setSidebarOpen(true)}
+        onMouseLeave={() => setSidebarOpen(false)}
       >
         <nav className="space-y-2 mt-4">
           {navigation.map((item) => {
@@ -51,12 +55,12 @@ export default function Sidebar({
                   "flex items-center gap-3  py-2 rounded-lg text-sm font-medium",
                   "transition-colors duration-200 mx-2",
                   "hover:bg-[var(--purple-bg4)] hover:text-white ",
-                  actualPage == item.href
+                  actualPage === item.href
                     ? "bg-[var(--purple-bg4)] text-[var(--dark-text1)] "
                     : "",
                   isSidebarOpen
-                  ? "px-4 transition-discrete duration-300 ease-out"
-                  : "px-4 transition-discrete duration-300 ease-out"
+                    ? "px-4 transition-discrete duration-300 ease-out"
+                    : "px-4 transition-discrete duration-300 ease-out"
                 )}
               >
                 <item.icon className="h-5 w-5 hover:scale-110" />
